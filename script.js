28a1212ec06c2029cfb0a83f40234e30f61b0f26
diegoalgibar <diegoalgibar123@gmail.com>
2023-08-30 15:32:39 -0300

    solucionado

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function saludar() {
+document.getElementById("saludar").addEventListener("click",(e)=>{
     alert("Hola!")
-}
+    e.stopPropagation();
+})
 document.getElementsByTagName("div")[0].addEventListener("click",()=>{
-alert("Hola! Soy el div")
+    
+    alert("Hola! Soy el div")
 })
